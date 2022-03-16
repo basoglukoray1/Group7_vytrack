5b73f4ca39e612613bd119c9e002535ec15b9277
kami guliyeva <kamala_guliyeva@yahoo.com>
2022-03-15 23:30:43 -0400

    Us_87 done

--- src/test/java/com/vytrack/tests/US_87_AccesstoOronicDocPage.java
+++ src/test/java/com/vytrack/tests/US_87_AccesstoOronicDocPage.java
@@ -1,6 +1,34 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class US_87_AccesstoOronicDocPage {
 
-    //step 1
+    @Test
+    public void accessOroincPage() {
+        //Driver.getDriver() --> driver
+
+        Driver.getDriver().get("https://qa2.vytrack.com/user/login");
+        WebElement usernameInput = Driver.getDriver().findElement(By.xpath("//input[@id='prependedInput']"));
+
+        usernameInput.sendKeys("Salesmanager125");
+        WebElement passwordInput = Driver.getDriver().findElement(By.xpath("//input[@id='prependedInput2']"));
+        passwordInput.sendKeys("UserUser123" + Keys.ENTER);
+
+
+        WebElement questionIcon = Driver.getDriver().findElement(By.xpath("//i[@title='Get help']"));
+        BrowserUtils.sleep(2);
+        questionIcon.click();
+
+
+        BrowserUtils.switchWindowAndVerify(Driver.getDriver(),"https://doc.oroinc.com/","Welcome to Oro Documentation");
+
+
+
+    }
 }
